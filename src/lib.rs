5a9d030df8018a93389360dc1fa68d08fb681ff7
Michael Neumann <mneumann@ntecs.de>
2015-12-27 12:33:47 +0100

    Add Default to ElementType trait. Required by lindenmayer-system for now

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub enum ExpressionError {
     InvalidOperation,
 }
 
-pub trait ElementType: Debug + Copy + Clone + PartialEq + PartialOrd {}
+pub trait ElementType: Debug + Copy + Clone + PartialEq + PartialOrd + Default {}
 
 pub trait Expression: Debug + Clone + PartialEq
 {
